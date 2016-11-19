4e76171d5460a44a0173a1fd2605e7daf622de4b
Agustin <carlosagustin_s@hotmail.com>
2016-11-19 17:19:47 -0300

    Mi app Django Girls, primer commit

---
+++ blog/migrations/0002_auto_20161119_1627.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='create_date',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='published_date',
+            field=models.DateTimeField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='text',
+            field=models.TextField(default=''),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='title',
+            field=models.CharField(max_length=200, default=''),
+        ),
+    ]
